ddf2191d2aa7c2bcd7c9f9992aeff072bb79b60c
Lucas Gruner <lucas.gruner@gmail.com>
2023-02-21 16:57:50 +0100

    first working draft

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks', to: 'tasks#index'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # UPDATE
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # DELETE
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
